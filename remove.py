af520c1a8f10fc1c26458b0fae66ff695a127717
nversahakyanc <nversahakyanc@gmail.com>
2025-09-05 21:00:54 +0400

    car title, invoice, mix ph

--- remove.py
+++ remove.py
@@ -1,7 +1,7 @@
 import os
 
 # Folder containing your images
-folder = r"SSN\images"
+folder = r"mix-photolook\images"
 
 # Allowed image extensions
 image_exts = [".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp", ".tiff"]
