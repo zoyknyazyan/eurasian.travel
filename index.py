d3a61061ed23309122fde2e5a1a02c6d13d5ef08
nversahakyanc <nversahakyanc@gmail.com>
2025-09-08 23:46:56 +0400

    business bill

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 
 # Set your root directory
-root_dir = 'business-utility-bill/'  # change this if needed
+root_dir = 'certificate/'  # change this if needed
 
 for subdir, _, files in os.walk(root_dir):
     for file in files:
