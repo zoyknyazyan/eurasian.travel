6ac7565c0d838f6bec232cb77e553fcfbf315eb1
nversahakyanc <nversahakyanc@gmail.com>
2025-09-10 01:29:02 +0400

    paytsub

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 
 # Set your root directory
-root_dir = 'passport-photolook/'  # change this if needed
+root_dir = 'reference/'  # change this if needed
 
 for subdir, _, files in os.walk(root_dir):
     for file in files:
