6ce4c5ba9d77b5f3a2b15217e6b9c67267a601a4
nversahakyanc <nversahakyanc@gmail.com>
2025-09-10 01:07:57 +0400

    passport

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 
 # Set your root directory
-root_dir = 'mortgage-statement/'  # change this if needed
+root_dir = 'passport-photolook/'  # change this if needed
 
 for subdir, _, files in os.walk(root_dir):
     for file in files:
